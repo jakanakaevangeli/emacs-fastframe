1077d79fe81a41ac23db3923b31e6a6a98ef74f5
jakanakaevangeli <jakanakaevangeli@chiru.no>
2021-04-23 02:38:35 +0200

    Use a closure rather than global variables

--- fastframe.el
+++ fastframe.el
@@ -25,70 +25,87 @@ frames.")
 (defvar fastframe--pool-current-count 0
   "Number of invisible frames in `fastframe--pool'.")
 
-(defvar fastframe--to-remove nil)
-(defvar fastframe--last-parameters nil)
+(defvar fastframe--timer nil)
 
 (defun fastframe-x-create-frame-with-faces (parameters)
   "Create and return a frame with frame parameters PARAMETERS.
 Like `x-create-frame-with-faces', but tries to reuse existing
 invisible frames from `fastframe--pool' with matching
 PARAMETERS."
-  (prog1
-      (if-let* ((as (assoc parameters fastframe--pool))
-                (frame (cadr as)))
-          (progn
-            (let ((inhibit-quit t))
-              (pop (cdr as))
-              (setq fastframe--pool-current-count
-                    (1- fastframe--pool-current-count)))
-            (make-frame-visible frame)
-            frame)
-        (x-create-frame-with-faces parameters))
-    (setq fastframe--to-remove t)
-    (setq fastframe--last-parameters parameters)))
+  (let ((as (assoc parameters fastframe--pool))
+        (frame nil))
+    (if as
+        (if (setq frame (cadr as))
+            (progn
+              (let ((inhibit-quit t))
+                (pop (cdr as))
+                (setq fastframe--pool-current-count
+                      (1- fastframe--pool-current-count)))
+              (make-frame-visible frame)))
+      (setq as (list parameters))
+      (push as fastframe--pool))
+    (unless frame
+      (setq frame (x-create-frame-with-faces parameters)))
 
-;;;###autoload
-(defun fastframe--timer-function ()
-  "Add a new invisible frame to pool if necessary.
-Is the number of frames in the pool is less than
-`fastframe-pool-size', add a new frame and run this function
-again in a timer. If `fastframe--to-remove' in non-nil, first
-remove a random frame that doesn't match
-`fastframe--last-parameters'."
+    (when-let* ((timer fastframe--timer))
+      (cancel-timer timer))
+    (let ((timer))
+      (setq
+       timer
+       (run-with-idle-timer
+        fastframe-idle-time t
+        (lambda ()
+          (while
+              (and
+               (eq timer fastframe--timer)
+               (progn
+                 (if (< fastframe--pool-current-count
+                        fastframe-pool-size)
+                     (prog1 t
+                       (fastframe--add-frame-to-assoc as))
+                   (when (fastframe--remove-random-from-pool as)
+                     (fastframe--add-frame-to-assoc as))
+                   (cancel-timer timer)
+                   (setq fastframe--timer nil)))
+               (sit-for fastframe-idle-time))))))
+      (setq fastframe--timer timer))
+    frame))
+
+(defun fastframe--remove-random-from-pool (assoc)
+  "Remove a random frame from the pool.
+Do nothing and return nil if it corresponds to ASSOC from
+`frame--pool'. Otherwise remove it and return t."
+  (let* ((rand (random fastframe--pool-current-count))
+         (tail fastframe--pool)
+         (deleted nil))
+    (while
+        (and
+         (consp tail)
+         (<= 0 rand)
+         (let ((len (length (cdar tail))))
+           (if (< rand len)
+               (prog1 nil
+                 (unless (eq (car tail) assoc)
+                   (delete-frame
+                    (let ((inhibit-quit t))
+                      (setq fastframe--pool-current-count
+                            (1- fastframe--pool-current-count))
+                      (pop (cdar tail))))
+                   (setq deleted t)))
+             (setq rand (- rand len)))))
+      (setq tail (cdr tail)))
+    deleted))
+
+(defun fastframe--add-frame-to-assoc (assoc)
+  "Add a frame with to cdr of ASSOC.
+car of ASSOC specifies the frames parameters. Increase
+`fastframe--pool-current-count'."
   (when (memq window-system '(x w32 ns))
-    (when (and fastframe--to-remove
-               (=> fastframe--pool-current-count fastframe-pool-size))
-      (let* ((keep (assoc fastframe--last-parameters fastframe--pool))
-             (rand (random
-                    (- fastframe--pool-current-count (length (cdr keep)))))
-             (tail fastframe--pool))
-        (while
-            (and
-             (consp tail)
-             (<= 0 rand)
-             (or (eq (car tail) keep)
-                 (let ((len (length (cdar tail))))
-                   (if (< rand len)
-                       (prog1 nil
-                         (delete-frame
-                          (let ((inhibit-quit t))
-                            (setq fastframe--pool-current-count
-                                  (1- fastframe--pool-current-count))
-                            (pop (cdar tail)))))
-                     (setq rand (- rand len))))))
-          (setq tail
-                (cdr tail)))))
-    (setq fastframe--to-remove nil)
     (when (< fastframe--pool-current-count fastframe-pool-size)
-      (let* ((params fastframe--last-parameters)
-             (frame (x-create-frame-with-faces
-                     (cons '(visibility . nil) params)))
-             (as (assoc params fastframe--pool)))
-        (unless as
-          (setq as (list params))
-          (push as fastframe--pool))
+      (let* ((frame (x-create-frame-with-faces
+                     (cons '(visibility . nil) (car assoc)))))
         (let ((inhibit-quit t))
-          (push frame (cdr as))
+          (push frame (cdr assoc))
           (setq fastframe--pool-current-count
                 (1+ fastframe--pool-current-count)))))))
 
